7b01e55d1fd2520e4bdbe4996c8945238f9adecf
Trenton Laule <trenton.laule@mavs.uta.edu>
2023-03-03 10:56:36 -0600

    updated list.h from professor

--- hw3/list.h
+++ hw3/list.h
@@ -1,5 +1,5 @@
 /* 
- * Updated 2/17/2022 - Alexandra Stefan
+ * Updated 3/2/2023 - Alexandra Stefan
  */
 
 
@@ -14,8 +14,7 @@ struct node {
 
 nodePT array_2_list(int arr[], int N);
     
-/* Creates a new node, that contains the value specified in the argument, 
- * and that points to next_in. 
+/* Creates a new node, with data value_in and NULL for next. 
  */
 nodePT new_node(int value_in);
 
@@ -28,44 +27,69 @@ check that the node is not NULL  */
 
 void destroy_list(nodePT L);
 
-// Iterates through list and counts nodes.
-int compute_length(nodePT L);  
-
 /* 
-Creates a new node and inserts it at the beginning of list (*La).
+Creates a new node with data d and inserts it at the beginning of list (*La).
+It modifies content of the memory that La points to.
 */ 
 void insert_front(struct node ** La, int d);
 
-/* Inserts new_node to the specified list, at the position right after
-  the node called "previous".
-  Inserting at the begining (as new first node) is not handled by this function.
- */
-void insert_node_after(nodePT previous, nodePT new_node);
+/* Inserts newP after the node prev.
+   Note that this is works on nodes. It does not matter how a list is represented.
+   prev is just a node.
+*/
+void insert_node_after(nodePT prev, nodePT newP);
 
-/* Inserts in list L, a the new node newP, after the node prev.
- If prev is NULL it means newP must be linked to the beginning of L
- It uses the list representation (L points to the first node with data)
+/* 
+   Parameters:
+	- L a linked list (has the address of the first node).
+	- prev - is either NULL or the address of a node node from list L. 
+	- newP - address of a node to be inserted/linked in L. 
+	         newP will be linked in L after prev.   
+    If prev is NULL it means newP must be linked to the beginning of L,
+	and will be the first node the list.
+	
+	Returns: the address of the first node in the updated list.
  */
 nodePT insert_node(nodePT L, nodePT prev, nodePT newP);
 
-/* Delete the node after the node "prev".
+/* Delete the node after the node prev.
    Note that this is works on nodes. It does not matter how a list is represented.
    prev is just a node.
 */
 void delete_node_after(nodePT prev);
 
-/* Deletes from list L, the node after prev. 
- If prev is NULL it means that the first node of L must be deleted.
- It uses the list representation (L points to the first node with data).
+/* 
+    Parameters:
+	- L a linked list (has the address of the first node).
+	- prev - is either NULL or the address of a node node from list L. 
+             If prev is NULL the first node of L will be deleted.
+	
+	Returns: the address of the first node in the updated list. 
  */
 nodePT delete_node(nodePT L, nodePT prev);
 
+// Swaps 2 nodes after prev.
+// If prev is NULL or not enough nodes for swapping, it does nothing.
+void swap_2_after(nodePT prev);
+
+/*
+ Iterates through list L and counts nodes.
+ Returns the length of list L.
+*/
+int compute_length(nodePT L);  
+
+
+
+// prints the linked list my_list
 void print_list_vert(nodePT my_list);
 
+// prints the linked list my_list
 void print_list_horiz(nodePT my_list);
 
-// for each node prints both data field and the mem address of that node
-// (not the field next, but address of the node)
+/*  Prints the nodes in list L. 
+	For each node prints both the data and the memory address of that node
+	(not the field next, but the address of the node)
+ */
 void print_list_pointer(nodePT my_list);
 
 #endif	/* LIST_H */
