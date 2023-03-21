bd0e6d258c242feb093502e97c99eac0123acf36
TrentonDL <trenton.laule@mavs.uta.edu>
2023-03-21 15:34:34 -0500

    working on heap.c

--- hw4/heap.c
+++ hw4/heap.c
@@ -7,26 +7,46 @@
 
 #define DEBUG 1
 //#define DEBUG 0
-int idx_of_max_Value(int *arr, int p, int left, int right, int N);
 
-/*
-// uncomment and write your code
+int idx_of_max_value(int *arr, int p, int left, int right, int N);
+
 struct heap_struct make_heap_empty(int cap){
+    struct heap_struct * heap;
+    heap->capacity = cap;
+    heap->N = 0;
+    heap->items = calloc(cap, sizeof(int));
+    heap->items = NULL;
+
+    return *heap;
 }
 
 struct heap_struct make_heap(int N, int * arr){
+    struct heap_struct heap;
+    heap.capacity = sizeof(*arr) - 1;
+    heap.N = N;
+    heap.items = arr;
+    
+    return heap;
 }
-*/
 
 void destroy(struct heap_struct * heapP){
-	// write your code here
+	free(heapP->items);
+    heapP->N = 0;
 }
-
 
 void print_heap(struct heap_struct heapS){
-	// write your code here
+    
+    printf("Heap:  size: %d, capacity: %d\n", heapS.N, heapS.capacity);
+    printf("indexes:");
+    for(int i = 0; i < heapS.N; i++){
+        printf("%6d,", i);
+    }
+    printf("\nvalues:");
+    for(int i = 0; i < heapS.N; i++){
+        printf("%6d,", heapS.items[i]);
+    }
+    printf("\n");
 }
-
 
 void swim_up(int idx, int * arr){
 	while((idx > 1) && (arr[idx] > arr[idx/2])){
@@ -36,12 +56,11 @@ void swim_up(int idx, int * arr){
     }
 }
 
-
 void sink_down(int i, int N, int * arr){
 	int left_idx = 2*i;
     int right_idx = (2*i)+1;
-    struct heap_struct heap = make_heap(N, arr);
-    int idx_max_value = peek(heap);
+    int idx_max_value = idx_of_max_value(arr, i, left_idx, right_idx, N);
+
     while((idx_max_value != i) && (idx_max_value<=N)){
         int temp = arr[i];
         arr[i] = arr[idx_max_value];
@@ -49,26 +68,11 @@ void sink_down(int i, int N, int * arr){
         i = idx_max_value;
         left_idx = 2*i;
         right_idx = (2*i)+1;
-
+        idx_max_value = idx_of_max_value(arr, i, left_idx, right_idx, N);
     }
 }
 
-void add(struct heap_struct * heapP, int new_item){
-	// write your code here
-}
-
-int peek(struct heap_struct heapS){
-	printf("\npeek placeholder, returns -1\n");
-	return -1;
-}
-
-
-int poll(struct heap_struct * heapP){
-	printf("\npoll placeholder, returns -1\n");
-	return -1;
-}
-
-int idx_of_max_Value(int *arr, int p, int left, int right, int N){
+int idx_of_max_value(int *arr, int p, int left, int right, int N){
     int idx_max_value = p;
 
     if((left<=N)&&(arr[left] > arr[idx_max_value])){
@@ -80,4 +84,28 @@ int idx_of_max_Value(int *arr, int p, int left, int right, int N){
     }
 
     return idx_max_value;
+}
+
+void add(struct heap_struct * heapP, int new_item){
+	if(heapP->capacity < heapP->N){
+        printf("Heap full cannot add item\n");
+    }
+    else{
+        int idx = heapP->N;
+        printf("idx created\n");
+        heapP->items[idx] = new_item;
+        printf("item added to end of heap\n");
+        swim_up(idx, heapP->items);
+        heapP->N++;
+    }
+}
+
+int peek(struct heap_struct heapS){
+	printf("\npeek placeholder, returns -1\n");
+	return -1;
+}
+
+int poll(struct heap_struct * heapP){
+	printf("\npoll placeholder, returns -1\n");
+	return -1;
 }
