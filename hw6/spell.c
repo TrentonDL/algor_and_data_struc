216660fb6fcc38e124cebef171faf2ae18d03953
TrentonDL <trenton.laule@mavs.uta.edu>
2023-04-17 14:01:15 -0500

    part 1 works

--- hw6/spell.c
+++ hw6/spell.c
@@ -43,34 +43,41 @@ int Dist(char * f, char * s, int i, int j){
 	return 1 + min(Dist(f, s, i, j-1), Dist(f, s, i-1, j), Dist(f, s, i-1, j-1));
 }
 
-void p_table(char * v_string, char * h_string, int arr[], int v, int h){
-	printf("  |");
-	for(int i=0;i<h; i++){
+void p_table(char * v_string, char * h_string, int * arr[], int v, int h){
+	printf("\n  |");
+	for(int i=0;i<=h; i++){
 		if(i == 0)
 			printf("   |");
 		else
 			printf("  %c|", h_string[i-1]);
 	}
-	printf("\n--");
-	for(int i=0;i<h; i++){
+	printf("\n---");
+	for(int i=0;i<=h; i++){
 		printf("----");
 	}
-	printf("  |");
+	printf("\n  |");
+
 	for(int j=0; j<=h; j++){
 		printf("  %d|", j);
 	}
+	printf("\n---");
+	for(int i=0;i<=h; i++){
+		printf("----");
+	}
+	printf("\n");
 	for(int k=0; k<v; k++){
 		printf(" %c|", v_string[k]);
 		printf("  %d|", k);
-		for(int c=0;c<=h; c++){
+		for(int c=0;c<h; c++){
 			printf("  %d|", arr[k][c]);
 		}
-		printf("\n--");
+		printf("\n---");
 		for(int c=0;c<=h; c++){
 			printf("----");
 		}
-		printf("\n\n");
+		printf("\n");
 	}
+	printf("\n");
 }
 
 void fill_dictonary(char** dictonary, char str[]){
@@ -92,29 +99,34 @@ Return:  the value of the edit distance
 int edit_distance(char * first_string, char * second_string, int print_table){
 	int f_length = strlen(first_string);
 	int s_length = strlen(second_string);
-	int (*edit_arr)[f_length] = calloc(s_length, f_length * sizeof(int));
+	int* edit_arr[f_length];
+	//int (*edit_arr)[f_length] = calloc(s_length, f_length * sizeof(int));
+	for(int i=0; i < f_length; i++){
+		edit_arr[i] =(int *)malloc(s_length * sizeof(int));
+	}
 
 
-	for(int i = f_length; i >= 0; i--){
-		for(int j = s_length; j >= 0; j--){
+	for(int i = f_length - 1; i >= 0; i--){
+		for(int j = s_length - 1; j >= 0; j--){
 			int equals = 0;
 
 			if(first_string[i] != second_string[j])
 				equals = 2;
 
-			edit_arr[i][j] = min(Dist(first_string, second_string, i-1, j) +1,
-								Dist(first_string, second_string, i, j-1) +1,
-								Dist(first_string, second_string, i-1, j-1) + equals);
+			edit_arr[i][j] = Dist(first_string, second_string, i, j);
+			//min(Dist(first_string, second_string, i-1, j) +1,
+			//					Dist(first_string, second_string, i, j-1) +1,
+			//					Dist(first_string, second_string, i-1, j-1) + equals);
 		}
 	}
 
 	if(print_table == 1){
-		p_table(first_string, second_string, *edit_arr, f_length, s_length);
+		p_table(first_string, second_string, edit_arr, f_length, s_length);
 	}
 
-	int edit_distance = Dist(first_string, second_string, f_length, s_length);
+	//Dist(first_string, second_string, f_length, s_length);
 
-	return edit_distance;  // replace this line with your code
+	return edit_arr[f_length-1][s_length-1];  // replace this line with your code
 }
 
 /*
