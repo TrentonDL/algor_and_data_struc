27f652d24b0f8dda7a5739bf0b2f7156e9d40234
TrentonDL <trenton.laule@mavs.uta.edu>
2023-05-01 13:37:25 -0500

    using a struct and is now dynamically allocated

--- hw7/courses_graph.c
+++ hw7/courses_graph.c
@@ -17,6 +17,12 @@
     // argv = input strings array
 FILE * openFile(int argc, char *argv[]);
 
+typedef struct CourseRequirements {
+    char *name;
+    char **prereqs;
+    int prereqCount;
+}CourseRequirements;
+
 int main(int argc, char *argv[]){
     FILE *fp = NULL;
     char fileline[FILELINE_SIZE] = {};
@@ -24,39 +30,48 @@ int main(int argc, char *argv[]){
     char *delims = " ";
     char *newline;
     int course_counter = 0;
-    char **courses[MAX_NUM_COURSES] = {};
+    CourseRequirements **courses = NULL;
     
-
     fp = openFile(argc, argv);
 
     while (fgets(fileline, FILELINE_SIZE, fp) != NULL){
+        char* vertex[] = {};
         char *newline = strchr(fileline, '\n');
         if(newline){
             *newline = 0;
         }
 
-        printf("i am here\n");
+        courses = (CourseRequirements**)realloc(courses, (course_counter+1) * sizeof(CourseRequirements*));
+        courses[course_counter] = malloc(sizeof(CourseRequirements));
         int vertex_counter = 0;
+
         Token = strtok(fileline, delims);
-        printf("Tokenized - |%s|\n", Token);
-        courses[course_counter][vertex_counter] = (char*) malloc((strlen(Token)+1) * sizeof(char));
-        printf("memory allocated\n");
-        strcpy(courses[course_counter][vertex_counter], Token);
-        printf("While loop\n");
-        vertex_counter++;
+        courses[course_counter]->name = malloc((strlen(Token)+1) * sizeof(char));
+        strcpy(courses[course_counter]->name, Token);
+        courses[course_counter]->prereqs = NULL;
+        courses[course_counter]->prereqCount = 0;
 
         do{
             Token = strtok(NULL, delims);
             if(Token != "\0" && Token != NULL){
-                courses[course_counter][vertex_counter] = (char*) malloc((strlen(Token)+1) * sizeof(char));
-                strcpy(courses[course_counter][vertex_counter], Token);
-                vertex_counter++;
-                printf("vertex counter - %d\n", vertex_counter);
+                courses[course_counter]->prereqs = (char**)realloc(courses[course_counter]->prereqs, (courses[course_counter]->prereqCount+1) * sizeof(char*));
+                courses[course_counter]->prereqs[courses[course_counter]->prereqCount] = (char*) malloc((strlen(Token)+1) * sizeof(char));
+                strcpy(courses[course_counter]->prereqs[courses[course_counter]->prereqCount], Token);
+                courses[course_counter]->prereqCount++;
             }
         }while(Token != "\0" && Token != NULL);
 
         course_counter++;
-        printf("course counter - %d\n", course_counter);
+    }
+
+    for (int i=0; i < course_counter; i++) {
+        printf("In print for %d\n", i);
+        printf("Course %s has the following prereqs:\n", courses[i]->name);
+        for (int j=0; j < courses[i]->prereqCount; j++) {
+            printf("%s\n", courses[i]->prereqs[j]);
+        }
+        printf("Finished prereqs\n");
+        printf("\n");
     }
     return EXIT_SUCCESS;
 }
