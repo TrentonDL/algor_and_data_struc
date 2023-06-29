6e7e5725c5aa52742875eccaab7802d490271781
TrentonDL <trenton.laule@mavs.uta.edu>
2023-06-29 17:54:52 -0500

    problem 4-6

---
+++ Summer_23/Homework_code/Homework3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int FunctionR(int n)
+{
+    if (n == 0)
+        return n;
+    else
+        return FunctionR(n-1) + n;
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    printf("%d\n", FunctionR(num));
+
+    return 0;
+}
