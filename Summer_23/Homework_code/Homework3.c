651238cbdbb411e7ac37052a85267c6815cfabbf
TrentonDL <trenton.laule@mavs.uta.edu>
2023-06-29 18:15:58 -0500

    problem 8

--- Summer_23/Homework_code/Homework3.c
+++ Summer_23/Homework_code/Homework3.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void FunctionR(int Z[], int a, int b)
+void FunctionR(int Z[], int a, int b, int e, int f)
 {
-    if(a < b)
-        return;
+    printf("%d-", Z[e]+Z[f]);
 
-    printf("%d-", Z[1]+Z[3]);
-    Z[2] = Z[4];
-
-    FunctionR(Z, Z[1], Z[3]);
+    if(a > b)
+    {
+        Z[e] -= Z[f];
+        FunctionR(Z, Z[f+1], Z[e+1], f, e);
+        printf("%d-", Z[f-1]+Z[e+1]);
+    }
 }
 
 int main()
 {
-    int A[] = {848, 123, 687, 981, 532};
+    int A[] = {1032, 687, 648, 765, 981};
+    int x = 2, y = 3;
 
-    FunctionR(A, A[2], A[4]);
+    FunctionR(A, A[0], A[1], x, y);
+    
     return 0;
 }
