40cb6de43459d2ed2b3b8d906fdbdd512a5bd023
TrentonDL <trenton.laule@mavs.uta.edu>
2023-06-29 10:59:09 -0500

    adding files and copied code from code 2

--- Summer_23/Coding_Assignments/Code3/Code3_1001669855.c
+++ Summer_23/Coding_Assignments/Code3/Code3_1001669855.c
@@ -57,6 +57,24 @@ int ReadFileIntoArray(int argc, char *argv[], int **AP)
     return counter;
 }
 
+void insertionSort(int * A, int n)
+{
+    int i, key, j;
+
+    for( j = 1; j <n; j++ )
+    {
+        key = A[j];
+        i = j - 1;
+
+        while (i >= 0 && A[i] > key)
+        {
+            A[i+1] = A[i];
+            i = i - 1;
+        }
+        A[i + 1] = key;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     clock_t start, end;
@@ -64,5 +82,28 @@ int main(int argc, char *argv[])
 
     int n = ReadFileIntoArray(argc, argv, &AP);
 
+    start = clock();
+    insertionSort(AP, n);
+    end = clock();
+    
+    #ifdef PRINTARRAY
+    printArray(AP, n);
+    #endif
+
+    printf("Processed %d records\n", n);
+    printf("Insertion Sort = %ld Tics\n",(end-start));
+    free(AP);
+    AP = NULL;
+    n = 0;
+
+    n = ReadFileIntoArray(argc, argv, &AP);
+
+    start = clock();
+    
+    end = clock();
+
+    printf("Merge Sort = %ld Tics\n",(end-start));
+    free(AP);
+    AP = NULL;
     return 0;
 }
