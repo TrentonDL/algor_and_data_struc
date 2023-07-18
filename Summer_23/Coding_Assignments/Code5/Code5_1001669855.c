deff10e14060d45fd4798efb3db0a534e7826ea1
TrentonDL <trenton.laule@mavs.uta.edu>
2023-07-18 12:19:41 -0500

    adding more stuff to start

--- Summer_23/Coding_Assignments/Code5/Code5_1001669855.c
+++ Summer_23/Coding_Assignments/Code5/Code5_1001669855.c
@@ -4,8 +4,32 @@
 
 #define MAX 50
 
-int main()
+struct Vertex
 {
+    int idx;
+    char Label;
+    int distance;
+    int previous;
+    int visted;
+};
 
+void openFile(FILE ** fp, char *filename)
+{
+	*fp = fopen(filename, "r");
+	if(*fp == NULL)
+	{
+		printf("File \"%s\" failed to open...exiting\n", filename);
+		exit(-1);
+	}
+}
+
+int main(int argc, char * argv[])
+{
+    if(argc < 2)
+	{
+		printf("File must be provided on command line...exiting\n");
+		exit(-1);
+	}
+    
     return 0;
 }
