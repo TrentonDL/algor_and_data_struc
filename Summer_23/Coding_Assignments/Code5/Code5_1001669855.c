9c842b172be45490254a336b3cd61da29ea86d4e
TrentonDL <trenton.laule@mavs.uta.edu>
2023-07-29 16:01:29 -0500

    dijkstra is in now just need to work on printing results

--- Summer_23/Coding_Assignments/Code5/Code5_1001669855.c
+++ Summer_23/Coding_Assignments/Code5/Code5_1001669855.c
@@ -82,6 +82,7 @@ int populateAdjMatrix(int AdjacencyMatrix[MAX][MAX], char * argv[], Vertex Verte
 		VertexArray[i].idx = i;
 		VertexArray[i].visited = 0;
 		VertexArray[i].distance = INT_MAX;
+		VertexArray[i].previous = -1;
 
 		Token = strtok(NULL, ",");
 
@@ -97,6 +98,70 @@ int populateAdjMatrix(int AdjacencyMatrix[MAX][MAX], char * argv[], Vertex Verte
 	}
 
 	return vertexCount;
+}
+
+int dijkstra(int AdjacencyMatrix[MAX][MAX], Vertex VertexArray[], int VertexCount, int start, int dest)
+{
+	int Current_idx = start;
+	VertexArray[start].distance = 0;
+	VertexArray[start].previous = -1;
+	VertexArray[start].visited = 1;
+
+	int x,y,z, dofu, cofuv, dofv, SmallestVertexIndex;
+	for(x = 0; x < VertexCount-1; x++)
+	{
+		for(y = 0; y < VertexCount; y++)
+		{
+			if(AdjacencyMatrix[Current_idx][y] != -1 && !(VertexArray[y].visited))
+			{
+				dofu = VertexArray[Current_idx].distance;
+				cofuv = AdjacencyMatrix[Current_idx][y];
+				dofv = VertexArray[y].distance;
+
+				if(dofu + cofuv < dofv)
+				{
+					dofv = dofu + cofuv;
+					VertexArray[y].distance = dofv;
+					VertexArray[y].previous = Current_idx;
+				}
+
+			}
+		}
+		SmallestVertexIndex = -1;
+		int SmallestVertex = INT_MAX;
+
+		for(z = 0; z < VertexCount; z++)
+		{
+			if(!VertexArray[z].visited)
+			{
+				if(VertexArray[z].distance < SmallestVertex)
+				{
+					SmallestVertex = VertexArray[z].distance;
+					SmallestVertexIndex = z;
+				}
+			}
+		}
+		Current_idx = SmallestVertexIndex;
+		VertexArray[Current_idx].visited = 1;
+	}
+	return VertexArray[dest].distance;
+}
+
+void printPath( Vertex VertexArray[], int start, int end, int vertexCount)
+{
+	int path[vertexCount];
+	int i=0 ,current_idx = end;
+	while(VertexArray[current_idx].previous != -1)
+	{
+		path[i] = VertexArray[current_idx].previous;
+		i++;
+	}
+	path[i] = VertexArray[current_idx].previous;
+	i++;
+	for(i; i>= 0; i--)
+	{
+		printf("%s->", VertexArray[i].label);
+	}
 }
 
 int main(int argc, char * argv[])
@@ -115,15 +180,27 @@ int main(int argc, char * argv[])
 	printArray(AdjacencyMatrix,vertexCount);
 	#endif
 
-	char starting_vertex[6];
-    char dest_vertex[6];
+	char starting_label[6];
+    char dest_label[6];
 	printf("What is the starting vertex? ");
-	scanf(" %s", starting_vertex);
+	scanf(" %s", starting_label);
 
 	printf("What is the destination vertex? ");
-	scanf(" %s", dest_vertex);
+	scanf(" %s", dest_label);
 
+	int i, start_idx = 0, dest_idx = 0;
+	for(i = 0; i < vertexCount; i++)
+	{
+		if(strcmp(starting_label, VertexArray[i].label) == 0)
+			start_idx = VertexArray[i].idx;
+		else if (strcmp(dest_label, VertexArray[i].label) == 0)
+			dest_idx = VertexArray[i].idx;
+	}
 
-
+	int dijkstra_len = dijkstra(AdjacencyMatrix, VertexArray, vertexCount, start_idx, dest_idx);
+	
+	printf("The path from %s to %s is ", VertexArray[start_idx].label, VertexArray[dest_idx].label);
+	printPath(VertexArray, start_idx, dest_idx, vertexCount);
+	printf(" and\nhas a length of %d\n", dijkstra_len);
     return 0;
 }
