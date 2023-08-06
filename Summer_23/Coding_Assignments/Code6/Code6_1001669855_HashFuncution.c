a4b962a58fdb637e0b8542688495f661ad71acd7
TrentonDL <trenton.laule@mavs.uta.edu>
2023-08-06 15:44:10 -0500

    completed hash and upload pt 1 pdf

--- Summer_23/Coding_Assignments/Code6/Code6_1001669855_HashFuncution.c
+++ Summer_23/Coding_Assignments/Code6/Code6_1001669855_HashFuncution.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
 #define HASTABLESIZE 10
 
-int HashFunction()
+int HashFunction(char HashValue[])
 {
-    
+    int string_len = strlen(HashValue);
+    int i, sum = 0;
+
+    for(i = 0; i<string_len; i++)
+    {
+        sum += HashValue[i];
+    }
+    //printf("%d\n", sum);
+    return sum % HASTABLESIZE;
 }
 
 int main()
 {
-
+    char HashValue[50] = {};
+    printf("Enter a name of a RollerCoaster: ");
+    scanf("%s", HashValue);
+    printf("The hash value for %s is %d\n", HashValue, HashFunction(HashValue));
 }
