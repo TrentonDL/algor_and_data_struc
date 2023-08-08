96c1c386d97cf36a36715b98e0a2203871166d72
TrentonDL <trenton.laule@mavs.uta.edu>
2023-08-07 21:51:02 -0500

    fixed scanf to fgets and changed input file from DOS to Linux

--- Summer_23/Coding_Assignments/Code6/Code6_1001669855_HashFuncution.c
+++ Summer_23/Coding_Assignments/Code6/Code6_1001669855_HashFuncution.c
@@ -1,3 +1,5 @@
+//Trenton Laule     1001669855//
+
 #include <stdio.h>
 #include <string.h>
 
@@ -6,6 +8,10 @@
 int HashFunction(char HashValue[])
 {
     int string_len = strlen(HashValue);
+
+    if(HashValue[string_len-1] == '\n');
+        HashValue[string_len-1] = '\0';
+    
     int i, sum = 0;
 
     for(i = 0; i<string_len; i++)
@@ -20,6 +26,6 @@ int main()
 {
     char HashValue[50] = {};
     printf("Enter a name of a RollerCoaster: ");
-    scanf("%s", HashValue);
+    fgets(HashValue, 50-1, stdin);
     printf("The hash value for %s is %d\n", HashValue, HashFunction(HashValue));
 }
